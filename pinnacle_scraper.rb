8e8cebe384b2841d8784ebcf1508f33f18f5a0b2
Jax <jax.hutton@outlook.com>
2020-03-09 14:22:19 -0400

    Enable CORS

--- pinnacle_scraper.rb
+++ pinnacle_scraper.rb
@@ -5,6 +5,19 @@ require 'bundler/setup'
 Bundler.require(:default)
 
 current_quarter = 3
+# Enable CORS
+set :bind, '0.0.0.0'
+configure do
+  enable :cross_origin
+end
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
+options '*' do
+  response.headers['Allow'] = 'GET'
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  200
+end
 
 get '/' do
   'Pinnacle Web Scraper'
